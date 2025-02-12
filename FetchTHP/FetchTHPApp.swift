232d5df1e68e872c72ace2db4af6492e077f0766
Oleksiy Petlyuk <oleksiy.petlyuk94@gmail.com>
2025-02-12 14:42:03 -0600

    Initial Commit

---
+++ FetchTHP/FetchTHPApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTHPApp.swift
+//  FetchTHP
+//
+//  Created by Petlyuk, Oleksiy on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchTHPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
